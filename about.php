48d331637bd46013bf0a354c1b5360c0e13b7ad1
Caleb Ness <45268686+CalebNessy@users.noreply.github.com>
2019-12-10 11:24:57 -0600

    Added new folders

--- about.php
+++ about.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Flyimals</title>
     <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
-    <link rel = "stylesheet" type = "text/css" href = "main.css" />
+    <link rel = "stylesheet" type = "text/css" href = "CSS/main.css" />
 </head>
 <body>
     <div class = "header txt">
@@ -14,7 +14,8 @@
         <button class="button" onclick="window.location.href='index.php'">Home</button>
         <button class="button" style="border: 2px solid #000;"  onclick="window.location.href='about.php'">About</button>
         <button id="login" onclick="window.location.href='signup.php'">Sign Up</button>
-        <button class="button">Contact</button>
+        <button class="button" onclick="window.location.href='products.php'">Products</button>
+        <button  class="button" onclick="window.location.href='contact.php'">Contact</button>
         <button id="login" onclick="window.location.href='login.php'">Login</button>
     </div>
     <div class = "content">
--- signup.php
+++ signup.php
@@ -6,16 +6,17 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Flyimals</title>
     <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
-    <link rel = "stylesheet" type = "text/css" href = "main.css" />
-    <link rel = "stylesheet" type = "text/css" href = "login.css" />
+    <link rel = "stylesheet" type = "text/css" href = "CSS/main.css" />
+    <link rel = "stylesheet" type = "text/css" href = "CSS/login.css" />
 </head>
 <body>
     <div class = "header txt">
-        <img src="/imgs/Logo.png" alt="Logo" class="logo">
+        <img src="imgs/Logo.png" alt="Logo" class="logo">
         <button class="button" onclick="window.location.href='index.php'">Home</button>
         <button class="button" onclick="window.location.href='about.php'">About</button>
+        <button class="button" onclick="window.location.href='products.php'">Products</button>
+        <button  class="button" onclick="window.location.href='contact.php'">Contact</button>
         <button id="login"  style="border: 2px solid #000;" onclick="window.location.href='signup.php'">Sign Up</button>
-        <button  class="button">Contact</button>
         <button id="login"  onclick="window.location.href='login.php'">Login</button>
     </div>
     <div class = "topmargin"></div>
