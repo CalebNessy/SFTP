4365d68797d8dfd0697cd35a7442e33b9303634b
Caleb Ness <45268686+CalebNessy@users.noreply.github.com>
2020-02-26 08:55:46 -0600

    Update orders.php

---
+++ orders.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Flyimals</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+    <link rel = "stylesheet" type = "text/css" href = "CSS/main.css" />
+</head>
+<body>
+    <?php
+        session_start();
+        // Check if the user is already logged in
+        $showhide = "hide";
+        if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+            $showhide = "show";
+        }else{
+            $showhide = "hide";
+        }
+    ?>
+    <div class = "header txt">
+        <img src="imgs/Logo.png" alt="Logo" class="logo">
+        <button class="button" onclick="window.location.href='index.php'">Home</button>
+        <button class="button" onclick="window.location.href='about.php'">About</button>
+        <button id="login" onclick="window.location.href='signup.php'">Sign Up</button>
+        <button class="button" onclick="window.location.href='products.php'">Products</button>
+        <button  class="button" onclick="window.location.href='contact.php'">Contact</button>
+        <button class="<?php echo $showhide ?> button" onclick="window.location.href='account.php'">Account</button>
+        <button id="login" onclick="window.location.href='login.php'"><?php
+            // Check if the user is already logged in
+            if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+                echo "Logout as ".$_SESSION['username'];
+            }else{
+                echo "Login";
+            }
+        ?></button>
+        <div class = "topmargin"></div>
+        <div class = "content txt">
+            <h3>My Orders:</h3>
+            <div class = "content txt">
+                <?php
+                //create variables for starting the server
+                $servername = "localhost:3306";
+                $db_username = "nfh_cness";
+                $db_password = "homeschool";
+                $db_database = "nfh_cness";
+                //Initialize the Database
+                $mySQLI = new mysqli($servername, $db_username, $db_password, $db_database);
+                if($mySQLI->connect_error){
+                    die("Connection Failed." . $mySQLI->connect_error);
+                }
+                $username = $_SESSION['username'];
+                $sql = "SELECT orderno, product, date FROM OrderHistory WHERE username = '$username'";
+                $result = mysqli_query($mySQLI, $sql);
+                $count = mysqli_num_rows($result);
+                if ($count > 0) {
+                    while($row = mysqli_fetch_assoc($result)) {
+                        echo "Product: " . $row["product"] . "<br>";
+                        echo "<u>" . "_______Date: " . $row["date"] . "_______" . "</u>" . "<br>";
+                    }
+                }else{
+                    echo "Any orders you make will appear here";
+                }
+                if($_SESSION['loggedin'] == false){
+                    header("Location: index.php");
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
